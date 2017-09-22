5c4cfbebcf01274378f1ac619ba1d5c1d29dc294
Shenghua <shenghua.lian@careerbuilder.com>
2017-09-22 17:27:47 +0800

    push files

---
+++ index.js
@@ -0,0 +1,258 @@
+// const Foo = { template: '<div>foo</div>' }
+// const Bar = { template: '<div>bar</div>' }
+
+// const routes = [
+//   { path: '/foo', component: Foo },
+//   { path: '/bar', component: Bar }
+// ]
+
+// const router = new VueRouter({
+//   routes // short for routes: routes
+// })
+
+
+
+
+albums_data = {
+	groups: [
+    		// {
+				// 	"link": "http://www.aimishe.cn/", 
+				// 	"backgroud": "http://s-media-cache-ak0.pinimg.com/111x55_sf-76/4a/86/2d/4a862d1c0c7258d95504583294b56824.jpg", 
+				// 	"english": "Home page", 
+				// 	"chinese": "首页"
+				// },
+        {
+					"link": "http://www.aimishe.cn/zuopin/renxiang/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/b3/7b/37/b37b37089451440486b914e2f33353b5.jpg", 
+					"english": "Portrait Photography", 
+					"chinese": "定制写真"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/hssy/",  
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/84/f4/43/84f4434ff5aa6fc90d4480496c94ae7a.jpg", 
+					"english": "Wedding Photography", 
+					"chinese": "婚纱情侣"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/genpai/",  
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/f3/66/c9/f366c9f74fdfc380775f6d993b52b3b8.jpg", 
+					"english": "Wedding Film", 
+					"chinese": "婚礼纪实"
+				},
+        {
+					"link": "http://www.aimishe.cn/zuopin/Pregnancy/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/89/65/5d/89655d03a5adb80ff77b09bf0cc4521a.jpg", 
+					"english": "Pregnancy", 
+					"chinese": "孕爱"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/qinzi/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/89/65/5d/89655d03a5adb80ff77b09bf0cc4521a.jpg", 
+					"english": "Family photos", 
+					"chinese": "亲子照"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/girlfriends/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/7a/46/2b/7a462b0d6b6440c778b50c48f090a1ef.jpg", 
+					"english": "Girl friends", 
+					"chinese": "闺蜜"
+				},
+        {
+					"link": "http://www.aimishe.cn/zuopin/shangye/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/44/fd/96/44fd9642a77c2ecbdb39c24a30c32d49.jpg", 
+					"english": "Commercial photography", 
+					"chinese": "商业摄影"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/live/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/50/19/59/501959300bb8e392a7c744727f06ca76.jpg", 
+					"english": "Life documentary", 
+					"chinese": "生活纪实"
+				},
+        {
+					"link": "http://www.aimishe.cn/jieshao/20141002/14121815875.html", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/0a/e3/f3/0ae3f383d128b4b4cb4cdd7dbf305c61.jpg", 
+					"english": "Introduction", 
+					"chinese": "介绍"
+				}
+
+    ],
+
+    albums: [
+    	{"link": "http://www.aimishe.cn/zuopin/renxiang/20161115/1479143994319.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161115/a68b7655b8f9eb61e3cf9a5a7b7beb5c.jpg", "name": "婷"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478987894317.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/98b4bcc034cbc0577a05095020117e93.jpg", "name": "莉"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478987748316.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/5b0dccaa878f3f75b9933f5c3589778c.jpg", "name": "莉"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478986284314.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/60823bfca57acd15cd292070195068fc.jpg", "name": "琳"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478985924313.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/cb4e7da459e69352dac1252eba1fe701.jpg", "name": "荣"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478984126312.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/e0f1e4928bfad7c701068d314e15e812.jpg", "name": "青"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478983030310.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/386cadf699b9b2eaa43fd2eb8d6c23ae.jpg", "name": "缘"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478982801309.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/1fe83ddac7ca910b57df1436f37b1018.jpg", "name": "缘"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478982381308.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/a599c282a4b16c507efccdfd3f29a354.jpg", "name": "烨"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478981015307.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/eb0665545b032caa31d163da7b196129.jpg", "name": "娇"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478980363306.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/ba72243bf2ac2283ec49a9a7203c861e.jpg", "name": "霜"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478980009305.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/db2ec14341c9c695a7c6c5702594bafd.jpg", "name": "婧"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478979240304.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/0cf3793552d8b5c40ab4584cc87e9f9d.jpg", "name": "贤"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478978332303.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/9c9d90749ec941c1a156c4b523443f65.jpg", "name": "岛"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478976841302.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/3dfdfbd5f977e17c2d2f3067928d268d.jpg", "name": "岛"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478975749301.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/09707fb86274ce24a6a8c4be6abc45b8.jpg", "name": "心怡"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478974988300.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/b53f0013b42fbfd0f7d25c02bdab3153.jpg", "name": "岛"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478973474299.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/fa695fea848f0f04ee4b7cc8a645cff7.jpg", "name": "岛"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478972162298.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/f28f09018a3462a49cdcd97e07e10776.jpg", "name": "遇见"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478971403296.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/773c20ed117f38ce27e1ef84d2afceb6.jpg", "name": "光"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478970494295.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/8c4c65a07e94a161b037c4c04091ed10.jpg", "name": "澜"},
+			{"link": "http://www.aimishe.cn/zuopin/renxiang/20161113/1478970309294.html", "coverLink": "http://www.aimishe.cn/d/file/p/20161113/0d4ffb9a92574d030f4515d215a016d4.jpg", "name": "菲"}
+		]
+  }
+
+
+album_data = {
+	"images": [
+		"/d/file/p/20161116/4dbcff9bd31b375c6d6125c19b69b2c3.jpg",
+		"/d/file/p/20161116/a3e5876b42cbb91d1df0f8f4bc6bb270.jpg",
+		"/d/file/p/20161116/bb93d5bd162d317561917021ca54ee79.jpg",
+		"/d/file/p/20161116/0a7209d8cf2bb3185b7867494914184c.jpg",
+		"/d/file/p/20161116/2dd84a49f640276b805ac244c97148ee.jpg",
+		"/d/file/p/20161116/6d6965c9c01c192f90b301966ec87d1d.jpg",
+		"/d/file/p/20161116/1a00b3ce8b36d1fe8755d69920863135.jpg",
+		"/d/file/p/20161116/6942d5e8a2a11b6c78c88d4f4010180e.jpg",
+		"/d/file/p/20161116/19b7d9018e05431a53c46ec9f3b13dd8.jpg",
+		"/d/file/p/20161116/51b2b2b6621dc44f0c14b28e331cfcd5.jpg",
+		"/d/file/p/20161116/b6646b71b6d862b158924aa50430f277.jpg",
+		"/d/file/p/20161116/cb1ac70cda993fbddf3dd6991b0a4ad1.jpg",
+		"/d/file/p/20161116/34044b3df5694a1dee263ccd6b7aa17e.jpg",
+		"/d/file/p/20161116/db7c85431c9e6d75a1d2ec03e09a1632.jpg",
+		"/d/file/p/20161116/db8fce548d0b07c678e3ff90df458f0e.jpg",
+		"/d/file/p/20161116/fd87828140f125565fc0d5638974ab2b.jpg",
+		"/d/file/p/20161116/9c13457030e8eaca90ea144451aa3b40.jpg",
+		"/d/file/p/20161116/60b8e8ddade62b8bdadda4336948865d.jpg"
+	],
+	"page_link": "http://www.aimishe.cn/zuopin/genpai/20161116/1479228017336.html",
+	"cover_link": "/d/file/p/20161116/4c70ac9e5a19b42861ec56bd85f14f39.jpg",
+	"name": "葛孜",
+	groups: [
+    		// {
+				// 	"link": "http://www.aimishe.cn/", 
+				// 	"backgroud": "http://s-media-cache-ak0.pinimg.com/111x55_sf-76/4a/86/2d/4a862d1c0c7258d95504583294b56824.jpg", 
+				// 	"english": "Home page", 
+				// 	"chinese": "首页"
+				// },
+        {
+					"link": "http://www.aimishe.cn/zuopin/renxiang/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/b3/7b/37/b37b37089451440486b914e2f33353b5.jpg", 
+					"english": "Portrait Photography", 
+					"chinese": "定制写真"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/hssy/",  
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/84/f4/43/84f4434ff5aa6fc90d4480496c94ae7a.jpg", 
+					"english": "Wedding Photography", 
+					"chinese": "婚纱情侣"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/genpai/",  
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/f3/66/c9/f366c9f74fdfc380775f6d993b52b3b8.jpg", 
+					"english": "Wedding Film", 
+					"chinese": "婚礼纪实"
+				},
+        {
+					"link": "http://www.aimishe.cn/zuopin/Pregnancy/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/89/65/5d/89655d03a5adb80ff77b09bf0cc4521a.jpg", 
+					"english": "Pregnancy", 
+					"chinese": "孕爱"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/qinzi/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/89/65/5d/89655d03a5adb80ff77b09bf0cc4521a.jpg", 
+					"english": "Family photos", 
+					"chinese": "亲子照"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/girlfriends/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/7a/46/2b/7a462b0d6b6440c778b50c48f090a1ef.jpg", 
+					"english": "Girl friends", 
+					"chinese": "闺蜜"
+				},
+        {
+					"link": "http://www.aimishe.cn/zuopin/shangye/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/44/fd/96/44fd9642a77c2ecbdb39c24a30c32d49.jpg", 
+					"english": "Commercial photography", 
+					"chinese": "商业摄影"},
+        {
+					"link": "http://www.aimishe.cn/zuopin/live/", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/50/19/59/501959300bb8e392a7c744727f06ca76.jpg", 
+					"english": "Life documentary", 
+					"chinese": "生活纪实"
+				},
+        {
+					"link": "http://www.aimishe.cn/jieshao/20141002/14121815875.html", 
+					"backgroud":"http://s-media-cache-ak0.pinimg.com/111x55_sf-76/0a/e3/f3/0ae3f383d128b4b4cb4cdd7dbf305c61.jpg", 
+					"english": "Introduction", 
+					"chinese": "介绍"
+				}
+
+    ]
+}
+
+const Albums = { 
+	template: `
+		<div>
+			<div class="guideBar">
+				<div v-for="group in groups" class="guild">
+						<a style="text-decoration: none" v-bind:href="group.link">
+							<img class="img-rounded" v-bind:src="group.backgroud" style="height: 3em; width: 90%">
+							<span class="guide-text">{{group.chinese}}</span>
+						</a>
+				</div>
+			</div>
+
+			<div class="row" style="margin: 5%">
+				<div v-for="album in albums" class="col-xs-6 col-sm-4 col-md-2 ">
+						<a v-bind:href="album.link" class="thumbnail">
+						<img v-bind:src="album.coverLink" v-bind:alt="album.name">
+					</a>
+				</div>
+			</div>
+		</div>
+	`,
+	data: function() {
+		return albums_data
+	}
+	
+}
+
+const Bar = { 
+	template: `
+		<div>
+			<div class="guideBar">
+				<div v-for="group in groups" class="guild">
+						<a style="text-decoration: none" v-bind:href="group.link">
+							<img class="img-rounded" v-bind:src="group.backgroud" style="height: 3em; width: 90%">
+							<span class="guide-text">{{group.chinese}}</span>
+						</a>
+				</div>
+			</div>
+			<div style="text-align: center; margin-top: 10px">
+				<div v-for="image_url in images" style="display: inline-block;">
+					<img class="img-responsive" v-bind:src="'http://www.aimishe.cn/' + image_url">
+				</div>
+			</div>
+		</div>
+	`,
+	data: function() {
+		return album_data
+	}
+}
+
+const router = new VueRouter({
+  routes: [
+		{ 
+			path: '/albums', 
+			component: Albums
+		},
+  	{ 
+			path: '/bar', 
+			component: Bar 
+		}
+	]
+})
+
+
+const app = new Vue({
+	el: '#app1',
+  router
+	// components: {
+  //   // <my-component> 将只在父模板可用
+  //   'my-component': Albums
+  // }
+})
